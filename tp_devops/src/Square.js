7ffb5b1fb50229e69749dae31220be5159067019
Quentin Monteferrario <quentin.monteferrario@fcinq.com>
2023-03-27 10:19:41 +0200

    Feature : display square X

---
+++ tp_devops/src/Square.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function Squarre() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+
+        export default function Square() {
+        return <button className="square">X</button>;
+    }
+    </div>
+  );
+}
+
+export default function Square() {
+    return <button className="square">X</button>;
+}
+
+export default Squarre;
